e06764cbf83a60b42d84d9a0f4c3de0578bf8049
Ashish Kumar Jha <79628513+ashish-kj@users.noreply.github.com>
2024-01-05 10:13:38 +0000

    [LeetCode Sync] Runtime - 7029 ms (5.00%), Memory - 146.2 MB (5.05%)

--- Solutions/0300-longest-increasing-subsequence/solution.py
+++ Solutions/0300-longest-increasing-subsequence/solution.py
@@ -1,34 +1,18 @@
+
 class Solution:
-#     def dfs(self, index, last_index, nums, dp):
-#         if index == 0:
-#             if last_index == -1 or nums[index] < nums[last_index]:
-#                 dp[(index, last_index)] = 1
-#                 return 1
-#             else:
-#                 dp[(index, last_index)] = 0
-#                 return 0
-        
-#         if dp[(index, last_index)] != -1:
-#             return dp[(index, last_index)]
-
-#         l1 = 0 + self.dfs(index - 1, last_index, nums, dp)
-#         l2 = -inf
-#         if last_index == -1 or nums[index] < nums[last_index]:
-#             l2 = 1 + self.dfs(index - 1, index, nums, dp)
-#         dp[(index, last_index)] = max(l1, l2)
-#         return dp[(index, last_index)]
-#     def lengthOfLIS(self, nums: List[int]) -> int:
-#         dp = {}
-#         for i in range(len(nums)):
-#             for j in range(-1, len(nums)):
-#                 dp[(i,j)] = -1
-#         return self.dfs(len(nums) - 1, -1, nums, dp)
-
     def lengthOfLIS(self, nums: List[int]) -> int:
-        total_number = len(nums)
-        dp = [0 for _ in range(total_number)]
-        for i in range(1, total_number):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp) + 1
+        n = len(nums)
+        dp = [[-1] * (n + 1) for _ in range(n)]
+        return self.f(0, -1, nums, n, dp)
+
+    def f(self, index, prevInd, nums, n, dp):
+        if index == n:
+            return 0
+        if dp[index][prevInd + 1] != -1:
+            return dp[index][prevInd + 1]
+        length = 0 + self.f(index + 1, prevInd, nums, n, dp)
+        if prevInd == -1 or nums[index] > nums[prevInd]:
+            length = max(length, 1 + self.f(index + 1, index, nums, n, dp))
+        dp[index][prevInd + 1] = length
+        return length
+
