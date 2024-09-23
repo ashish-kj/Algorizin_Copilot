2b958e60419870a6ee902688e820d35fac29c821
Ashish Kumar Jha <79628513+ashish-kj@users.noreply.github.com>
2024-09-23 16:39:45 +0000

    [LeetCode Sync] Runtime - 206 ms (12.16%), Memory - 17 MB (99.88%)

--- Solutions/2755-extra-characters-in-a-string/solution.py
+++ Solutions/2755-extra-characters-in-a-string/solution.py
@@ -1,19 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.dp = [-1] * 51  # Initialize dp array with -1 values
-        
-    def minExtraChar(self, s, dictionary):
-        return self.minExtraCharHelper(s, dictionary, 0)
-    
-    def minExtraCharHelper(self, s, dictionary, i):
-        if i == len(s):
-            return 0
-
-        if self.dp[i] == -1:
-            self.dp[i] = 1 + self.minExtraCharHelper(s, dictionary, i + 1)  # Initialize with one extra character.
-
-            for w in dictionary:
-                if s[i:i+len(w)] == w:
-                    self.dp[i] = min(self.dp[i], self.minExtraCharHelper(s, dictionary, i + len(w)))  # Update if a word in the dictionary is found.
-
-        return self.dp[i]  # Return the minimum extra characters starting from position i
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n, dictionary_set = len(s), set(dictionary)
+        @cache
+        def dp(start):
+            if start == n:
+                return 0
+            ans = dp(start + 1) + 1
+            for end in range(start, n):
+                curr = s[start: end + 1]
+                if curr in dictionary_set:
+                    ans = min(ans, dp(end + 1))
+            return ans
+            
+        return dp(0)
