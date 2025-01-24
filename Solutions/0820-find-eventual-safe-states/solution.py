d78fb2f458a1cbc83aab0ba09fbf71869951d2fb
Ashish Kumar Jha <79628513+ashish-kj@users.noreply.github.com>
2025-01-24 00:02:48 +0000

    [LeetCode Sync] Runtime - 59 ms (33.25%), Memory - 23.6 MB (59.93%)

--- Solutions/0820-find-eventual-safe-states/solution.py
+++ Solutions/0820-find-eventual-safe-states/solution.py
@@ -1,23 +1,34 @@
 class Solution:
-    def isSafe(self, s, graph, visited, checkCycle):
-        if visited[s] == 1:
-            return True  # Node is already visited and safe
-        if visited[s] == -1:
-            return False  # Node is visited and part of a cycle
-
-        visited[s] = -1  # Mark the current node as visited and part of a cycle
-        for it in graph[s]:
-            if not self.isSafe(it, graph, visited, checkCycle):
-                return False  # If any adjacent node is not safe, the current node is not safe
-
-        visited[s] = 1  # Mark the current node as visited and safe
-        return True
-
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        v = len(graph)
-        visited = [0] * v
-        checkCycle = []
-        for i in range(v):
-            if self.isSafe(i, graph, visited, checkCycle):
-                checkCycle.append(i)  # Add the nodes that are safe (eventual safe nodes) to the result
-        return checkCycle
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = deque()
+        # Push all the nodes with indegree zero in the queue.
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                # Delete the edge "node -> neighbor".
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+
+        return safeNodes
