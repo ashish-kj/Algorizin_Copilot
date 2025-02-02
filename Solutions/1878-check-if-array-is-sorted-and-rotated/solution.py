556e6233ed8a10e2f940262a09c4ecef7e321d2a
Ashish Kumar Jha <79628513+ashish-kj@users.noreply.github.com>
2025-02-02 16:56:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (50.76%)

---
+++ Solutions/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def check(self, nums):
+
+        n = len(nums)
+        count = 1
+
+        for i in range(1, 2 * n):
+            if nums[(i - 1) % n] <= nums[i % n]:
+                count += 1
+            else:
+                count = 1
+            if count == n:
+                return True
+                
+        return n == 1
+
